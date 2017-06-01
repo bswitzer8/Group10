8ec874a8d15fdaaf3763c023d748df6170fd4149
ben <bswitzer8@gmail.com>
2017-06-01 06:15:10 -0500

    I mean, it is a start

---
+++ app/scripts/main-controller.js
@@ -0,0 +1,13 @@
+app.controller("mainController", function($scope){
+ 
+    // read products
+    $scope.test = function(){
+ 
+        $scope.blah = "angular stuff";
+ 
+    };
+
+    $scope.test();
+     
+
+});
---
+++ db_scripts/listastic.sql
@@ -0,0 +1,25 @@
+
+CREATE DATABASE IF NOT EXISTS listastic;
+ 
+USE listastic;
+
+CREATE TABLE IF NOT EXISTS `users` (
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`name` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`password` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `lists` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` INT UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `description` TEXT DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `UserID`(`user_id` ASC),
+  CONSTRAINT `user_id` FOREIGN KEY(`user_id`) REFERENCES `listastic`.`users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE 
+);
