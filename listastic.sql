1449af835d471c37ad4ee7e12f2364f87a70c46f
ben <bswitzer8@gmail.com>
2017-06-06 06:30:21 -0500

    fixed the join table.

---
+++ listastic.sql
@@ -0,0 +1,40 @@
+
+CREATE DATABASE IF NOT EXISTS listastic;
+ 
+USE listastic;
+
+CREATE TABLE IF NOT EXISTS `users` (
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`name` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`password` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `lists` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` INT UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `description` TEXT DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `UserID`(`user_id` ASC),
+  CONSTRAINT `user_id` FOREIGN KEY(`user_id`) REFERENCES `listastic`.`users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE 
+);
+
+CREATE TABLE IF NOT EXISTS `sharedlists` (
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`user_id` INT UNSIGNED NOT NULL,
+	`list_id` INT UNSIGNED NOT NULL,
+	PRIMARY KEY (`id`),
+  	INDEX `UserID`(`user_id` ASC),
+  	INDEX `ListID` (`list_id` ASC),
+  	FOREIGN KEY(`user_id`) REFERENCES `listastic`.`users`(`id`) ON UPDATE CASCADE, 
+  	FOREIGN KEY(`list_id`) REFERENCES `listastic`.`lists`(`id`) ON UPDATE CASCADE
+);
+
+
+
+!-- INSERT INTO users (name, email, password) values ('ben', 'bswitzer8@gmail.com', 'butts');
