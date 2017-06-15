3474e070227de9dfcd772756ddfaf39d08eadf02
ben <bswitzer8@gmail.com>
2017-06-14 22:10:14 -0500

    The routing is working. Angular stuff is doing angular stuff.

---
+++ app/scripts/mainController.js
@@ -0,0 +1,22 @@
+(function(){
+	/* global angular */
+	var app = angular.module("Listastic"); 
+
+	app.controller("mainController", function($scope, $http){
+		console.log("testing from main");
+		
+		// we would get the user like the data
+		$scope.user = { name: "bob"};
+			// this needs to be changed to get from a php source
+			$http.get('app/content/json/dumby_data.json')
+			.then(function(res){
+				$scope.raw = res.data;  
+				$scope.todo = angular.copy($scope.raw);
+		});
+
+		$scope.tags = ["school", "fitness", "work"];         
+
+
+	});
+
+})();
