f558b19e1fec9a8ed8888123dcc3c24d32da2236
Ben <bswitzer8@gmail.com>
2017-06-26 19:34:49 -0500

    Added comments

--- logout.php
+++ logout.php
@@ -1,3 +1,15 @@
+/* 
+  Okay, so with the way I'm going to do this. we will actually only need 
+ <?php
+session_start();
+session_unset();
+session_destroy(); 
+echo "User has been logged out";
+?>
+cause this will be a call to a php script.
+
+*/
+
 <?php
 /* Log out process, unsets and destroys session variables */
 session_start();
