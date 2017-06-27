ce446cdbe2988b79f11d15d6520bb99d97fef079
Natalie Weinstein <natalie_weinstein@ultimatesoftware.com>
2017-06-26 23:23:38 -0400

    Makes login routing work

--- login.php
+++ login.php
@@ -9,22 +9,21 @@ $result = $conn->query("SELECT * FROM users WHERE email='$email'");
 if ( $result->num_rows == 0 ){ // User doesn't exist
     // Ben: urgh, I'll handle this whole thing.
     $_SESSION['message'] = "User with that email doesn't exist!";
-    header("location: error.php");
+    echo "<script>window.location.replace('error.php')</script>";
 }
 else { // User exists
     $user = $result->fetch_assoc();
 
     if ( $_POST['password'] == $user['password'] ) {
 
-        // Ben: No pls. just do $_SESSION['user_id'] = $user["id"];
         $_SESSION['user_id'] = $user["id"];
 
-        header("location: app/views/main.html");
+        echo "<script>window.location.replace('../cop4813')</script>";
     }
     else {
         // Ben: urgh, I'll handle this whole thing.
         $_SESSION['message'] = "You have entered wrong password, try again!";
-        header("location: error.php");
+        echo "<script>window.location.replace('error.php')</script>";
     }
 }
 
